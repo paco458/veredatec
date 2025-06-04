df86485dd862d9d5f6d2ed1418af7bfea93cb89d
paco458 <salazarcustodiojeferson123@gmail.com>
2025-06-03 22:34:12 -0500

    Dia1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FAB from './components/FAB';
 
 export default function App() {
+  const[count, setCount] = useState(10);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHude}> {count} </Text>
+      <FAB label='+1'
+      onLongPress={() => setCount(0)}//va a inicializar el contador a 0 cuando se mantenga presionado
+      onPress={() => setCount(count + 1)}//cuando se presiona el boton, se incrementa el contador
+      position='right' //posicion del boton, se puede cambiar a left, top, bottom
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textHude: {
+    fontSize: 120,
+    fontWeight: '100',
+  },
+  
 });
